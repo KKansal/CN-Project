acac75a67b41c1244a87b561eaa1823ad469af71
Keshav K <kansalkeshav@gmail.com>
2019-10-22 15:36:58 +0530

    Added Multi-threading part to test.py

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+import os
 from socket import *
 from time import sleep
+import threading
+
 rootDirectory="\\"
-serverPort = 12000
+serverPort = 12001
 
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(("",serverPort))
@@ -9,7 +12,11 @@ serverSocket.listen(1)
 print("The server is ready to receive")
 
 def serverequest(connectionSocket,requestheader):
-	if(requestheader[0]=="GET"):
+	# if(requestheader[0]=="GET"):
+	# 	available_files=os.listdir()
+	# 	if(requestheader[1] not in available_files):
+	# 		connectionSocket.send('HTTP/1.1 404 FILE NOT FOUND\n'.encode())
+	# 	else:
 		connectionSocket.send('HTTP/1.0 200 OK\n'.encode())
 		connectionSocket.send('Content-Type: text/html\n'.encode())
 		connectionSocket.send('\n'.encode())
@@ -21,23 +28,23 @@ def serverequest(connectionSocket,requestheader):
 	    	</html>""".encode())
 
 
+while True :
+	connectionSocket, addr = serverSocket.accept()
+	print("Connection accepted")
+	sentence = connectionSocket.recv(1024)
+	httprequest = sentence.decode()
+	print(httprequest)
+	httprequest=httprequest.split('\r\n')
+	for i in range(len(httprequest)):
+		httprequest[i]=httprequest[i].split()
 
-connectionSocket, addr = serverSocket.accept()
-print("Connection accepted")
-sentence = connectionSocket.recv(1024)
-httprequest = sentence.decode()
-print(httprequest)
-httprequest=httprequest.split('\r\n')
-
-
-for i in range(len(httprequest)):
-	httprequest[i]=httprequest[i].split()
-
-serverequest(connectionSocket,httprequest[0])
-
-connectionSocket.shutdown(SHUT_WR)
-connectionSocket.close()
-print("Connection Closed")
+	t1=threading.Thread(target=serverequest,args=(connectionSocket,httprequest[0],))
+	t1.start()
+	# serverequest(connectionSocket,httprequest[0])
+	t1.join()
+	connectionSocket.shutdown(SHUT_WR)
+	connectionSocket.close()
+	print("Connection Closed")
 serverSocket.shutdown(SHUT_RDWR)
 serverSocket.close()
 
---
+++ test_threading.py
@@ -0,0 +1,7 @@
+from threading import Thread as thread
+
+
+def func():
+	print('kk')
+
+thread(func())
