d6b615b28d1719226106c0910f0ac3b83d594525
ayrus-surya <suryaravi.15@gmail.com>
2019-10-23 19:04:39 +0530

    added support for image transfer through compression

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import os
+import gzip
 from socket import *
 from time import sleep
 import threading
 
-rootDirectory="/home/keshavk/Documents/CN Project/Templates1/"
-serverPort = 12000
+rootDirectory="./"
+serverPort = 1246
 
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(("",serverPort))
@@ -23,38 +24,52 @@ def serverequest(connectionSocket,requestheader):
 		if(len(requestheader[1])==1):
 			requestheader[1]="index.html"
 
-		# if(requestheader[1] not in  os.listdir(rootDirectory)):
-		# 	connectionSocket.send('HTTP/1.1 404 FILE NOT FOUND\n'.encode())
-		# else:
-		connectionSocket.send('HTTP/1.0 200 OK\n'.encode())
-		connectionSocket.send('Content-Type: text/html\n'.encode())
-		connectionSocket.send('\n'.encode())
-		send_file(connectionSocket,requestheader[1])
-		# connectionSocket.send("""
-	 #    	<html>
-	 #    	<body>
-	 #    	<h1>Hello KK</h1> this is my server!
-	 #    	</body>
-	 #    	</html>""".encode())
+		if(requestheader[1] not in  os.listdir()):
+		 	connectionSocket.send('HTTP/1.1 404 FILE NOT FOUND\n'.encode())
+		if(requestheader[1]=="/kk.html"):	
+			connectionSocket.send('HTTP/1.0 200 OK\n'.encode())
+			connectionSocket.send('Content-Type: text/html\n'.encode())
+			connectionSocket.send('\n'.encode())
+			send_file(connectionSocket,requestheader[1])
+		elif(requestheader[1].endswith("png") or requestheader[1].endswith("jpg") or requestheader[1].endswith("jpeg")):
+			try:
+				#compress image
+				if not (requestheader[1][1:]+'.gz' in os.listdir()):
+					with open(requestheader[1][1:],"rb") as img:
+						data = img.read()
+					bindata = bytearray(data)
+					with gzip.open(requestheader[1][1:]+'.gz', "wb") as f:
+						f.write(bindata)
+				
+				#read compressed image
+				with gzip.open(requestheader[1][1:]+'.gz', "rb") as imageFile:
+					s=imageFile.read()
+				
+				connectionSocket.send('HTTP/1.0 200 OK\n'.encode())
+				connectionSocket.send('Content-Type: image/png\n'.encode())
+				connectionSocket.send('\n'.encode())
+				connectionSocket.send(s)
+			except:
+				pass
 
+try:
+	while True :
+		connectionSocket, addr = serverSocket.accept()
+		print("Connection accepted")
+		sentence = connectionSocket.recv(1024)
+		httprequest = sentence.decode()
+		print(httprequest)
+		httprequest=httprequest.split('\r\n')
+		for i in range(len(httprequest)):
+			httprequest[i]=httprequest[i].split()
 
-while True :
-	connectionSocket, addr = serverSocket.accept()
-	print("Connection accepted")
-	sentence = connectionSocket.recv(1024)
-	httprequest = sentence.decode()
-	print(httprequest)
-	httprequest=httprequest.split('\r\n')
-	for i in range(len(httprequest)):
-		httprequest[i]=httprequest[i].split()
-
-	t1=threading.Thread(target=serverequest,args=(connectionSocket,httprequest[0],))
-	t1.start()
-	serverequest(connectionSocket,httprequest[0])
-	t1.join()
-	connectionSocket.shutdown(SHUT_WR)
-	connectionSocket.close()
-	print("Connection Closed")
-serverSocket.shutdown(SHUT_RDWR)
-serverSocket.close()
-
+		t1=threading.Thread(target=serverequest,args=(connectionSocket,httprequest[0],))
+		t1.start()
+		serverequest(connectionSocket,httprequest[0])
+		t1.join()
+		connectionSocket.shutdown(SHUT_WR)
+		connectionSocket.close()
+		print("Connection Closed")
+except KeyboardInterrupt:
+	serverSocket.shutdown(SHUT_RDWR)
+	serverSocket.close()
